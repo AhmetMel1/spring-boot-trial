0d4cd7ebc61376538bb762a787dd5d0bf87c8c80
AhmetMel <ahmet.melih0056@gmail.com>
2023-03-15 11:21:58 +0300

    Project updated

---
+++ src/main/java/springboot/rentACar/core/utilities/RestApi/Showroom.java
@@ -0,0 +1,11 @@
+package springboot.rentACar.core.utilities.RestApi;
+
+import lombok.Data;
+
+@Data
+public class Showroom {
+    private Object id;
+    private String name;
+    private String phoneNumber;
+    private String ownerName;
+}
