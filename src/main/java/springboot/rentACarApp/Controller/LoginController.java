f883f6e5f3c155ea23264b7cdeeb8746b70a25dc
AhmetMel <ahmet.melih0056@gmail.com>
2023-03-24 16:34:48 +0300

    Update project

--- src/main/java/springboot/rentACarApp/Controller/LoginController.java
+++ src/main/java/springboot/rentACarApp/Controller/LoginController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/login")
 @AllArgsConstructor
